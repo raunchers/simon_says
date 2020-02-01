c04eb27a021eb34bae38e5b33ed39abef4f5c31f
Michael Ethridge <maethridge@gmail.com>
2020-01-31 21:02:29 -0500

    Worked on the game code, program fails while user is inputting a sequence

--- game.js
+++ game.js
@@ -15,34 +15,25 @@ var level = 0;
 gamePattern.push(randomChosenColor);
 
 // Detect when a keyboard key has been pressed to start the game
-$(document).on("keypress", function(key){
+$(document).on("keypress", function(key) {
 
-    // Check if game has been started
-    if(gameStarted != true){
-
-      // Get a color from the computer
-      randomChosenColor = buttonColors[nextSequence()];
-
-      // Remove the empty string from the array
-      gamePattern.pop();
-      gamePattern.push(randomChosenColor);
-
-      // Choose the correct button
-      $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
-      playSound(randomChosenColor);
-
-      gameStarted = true;
-
-    }
+  // Check if game has been started
+  if (!gameStarted) {
+    nextSequence();
+    gameStarted = true;
+  }
 });
 
 // Detect when any of the buttons are pressed
-$("#green, #red, #yellow, #blue").click(function() {
+$(".btn").click(function() {
+
   var userChosenColor = this.id;
   userClickedPattern.push(userChosenColor);
+
   animatePress(userChosenColor);
   playSound(userChosenColor);
   checkAnswer(userClickedPattern.length - 1);
+
 });
 
 // Randomly generates a new sequence
@@ -53,34 +44,56 @@ function nextSequence() {
   //Update the h1 to show the current level
   $("#level-title").text("Level " + level);
 
-  return randomNumber = Math.floor(Math.random() * 4);
+  var randomNumber = Math.floor(Math.random() * 4);
+
+  // Get a color from the computer
+  randomChosenColor = buttonColors[randomNumber];
+
+  // Start of the game
+  // gamePattern keeps initalizing with an empty string
+  if (level == 1) {
+    gamePattern.pop();
+  }
+
+  gamePattern.push(randomChosenColor);
+
+  // Choose the correct button
+  $("#" + randomChosenColor).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColor);
 }
 
 // Plays the correct audio for the computer's and player's choices
-function playSound(name){
-    var clickedAudio = new Audio("sounds/" + name + ".mp3");
-    clickedAudio.play();
+function playSound(name) {
+  var clickedAudio = new Audio("sounds/" + name + ".mp3");
+  clickedAudio.play();
 }
 
 // Animates the button the user clicks on
-function animatePress(currentColor){
+function animatePress(currentColor) {
   $("#" + currentColor).addClass("pressed");
 
   // Wait 100ms then remove the pressed classes
-  setTimeout(function(){
+  setTimeout(function() {
     $("#" + currentColor).removeClass("pressed");
   }, 100);
 }
 
-function checkAnswer(currentLevel){
+function checkAnswer(currentLevel) {
 
-  //Check if pattern matches computers game's pattern
-  if(userClickedPattern[currentLevel] == gamePattern[currentLevel]){
-    setTimeout(function(){
-      nextSequence();
-    }, 1000);
-    userClickedPattern = [];
-  }else{
+  // Check that the user is inputting the correct sequence
+  if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+    console.log("Success");
+    console.log("gamePattern length: " + gamePattern.length);
 
+    // Check that the user is done inputting a sequence
+    if (userClickedPattern.length == gamePattern.length) {
+      setTimeout(function() {
+        nextSequence();
+      }, 1000);
+    }
+
+  } else {
+    console.log("failure");
   }
+
 }
